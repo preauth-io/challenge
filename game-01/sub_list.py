3fe4b6844dfb66a6986a02db9eefe26d969c2316
Roger Mendez <roger.mendez.r@gmail.com>
2022-02-08 22:07:47 -0400

    challenge answered

--- game-01/sub_list.py
+++ game-01/sub_list.py
@@ -1,5 +1,12 @@
 
 def generate_sub_list(list, num):
+    """
+    receiving function
+    :param list: type list
+    :param num: type int
+    returns a list
+    :return: list
+    """
     for first_index, value in enumerate(list):
         for second_index in range (first_index+1, len(list)): 
             sum = list[first_index] + list[second_index]
